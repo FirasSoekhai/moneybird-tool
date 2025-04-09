46046a6e9769f4e26757d513896bd10bb0a8b8e9
FirasSoekhai <firassoekhai@hotmail.nl>
2025-04-09 11:23:09 +0200

    Admin model aangemaakt

--- moneybird-tool/app/Models/Admin.php
+++ moneybird-tool/app/Models/Admin.php
@@ -2,9 +2,21 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Admin extends Model
 {
-    //
+    use HasFactory;
+
+    protected $primaryKey = 'admin_id';
+
+    protected $fillable = [
+        'user_id',
+        'admin_level',
+    ];
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id', 'user_id');
+    }
 }
