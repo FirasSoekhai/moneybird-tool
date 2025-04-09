81d35b4fc4954a246a52c9fc2b59825dc4365db6
FirasSoekhai <firassoekhai@hotmail.nl>
2025-04-09 11:14:15 +0200

    Admin model aangemaakt

--- moneybird-tool/app/Models/User.php
+++ moneybird-tool/app/Models/User.php
@@ -6,43 +6,38 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-
+use Laravel\Sanctum\HasApiTokens;
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
+    protected $primaryKey = 'user_id';
+
     protected $fillable = [
-        'name',
-        'email',
+        'username',
         'password',
+        'isVerified',
+        'admin_id',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
+
+
     protected $hidden = [
         'password',
-        'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
+    protected $casts = [
+        'isVerified' => 'boolean',
+        'password' => 'hashed',
+    ];
+    public function admin()
     {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
+        return $this->hasOne(Admin::class, 'user_id', 'user_id');
+    }
+
+    public function isAdmin()
+    {
+        return $this->admin()->exists();
     }
 }
