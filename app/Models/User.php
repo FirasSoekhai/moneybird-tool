518b73ee0c28cdc04d4798cfbd2a55523d00111c
FirasSoekhai <firassoekhai@hotmail.nl>
2025-04-15 10:54:41 +0200

    User model en migration aangepast

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,6 +13,7 @@ use Laravel\Sanctum\HasApiTokens;
 class User extends Authenticatable
 {
     use HasApiTokens;
+    protected $primaryKey = 'user_id';
 
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory;
