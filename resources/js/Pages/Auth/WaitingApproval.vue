ab89035edfafa4ee8a19df8e794aa9fa386882e5
FirasSoekhai <firassoekhai@hotmail.nl>
2025-04-29 10:27:14 +0200

    WaitingApproval.vue toegevoegd voor gebruikers die nog niet goedgekeurd zijn

---
+++ resources/js/Pages/Auth/WaitingApproval.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="text-center p-10">
+      <h1 class="text-3xl font-bold mb-4">Je account wordt beoordeeld</h1>
+      <p class="text-gray-600">Zodra een admin je goedkeurt, krijg je toegang tot het dashboard.</p>
+    </div>
+  </template>
