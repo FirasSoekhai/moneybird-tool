5a25eca93ae93ca1f722ecf3961411daef420a03
FirasSoekhai <firassoekhai@hotmail.nl>
2025-04-29 10:31:36 +0200

    AuthenticatedLayout toegevoegd zodat het dashboard nogsteeds bruikbaar is

--- resources/js/Pages/Auth/WaitingApproval.vue
+++ resources/js/Pages/Auth/WaitingApproval.vue
@@ -1,6 +1,12 @@
 <template>
-    <div class="text-center p-10">
+    <AuthenticatedLayout>
+        <div class="text-center p-10">
       <h1 class="text-3xl font-bold mb-4">Je account wordt beoordeeld</h1>
       <p class="text-gray-600">Zodra een admin je goedkeurt, krijg je toegang tot het dashboard.</p>
-    </div>
+    </div> 
+    </AuthenticatedLayout>
   </template>
+   
+  <script setup>
+  import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+</script>
